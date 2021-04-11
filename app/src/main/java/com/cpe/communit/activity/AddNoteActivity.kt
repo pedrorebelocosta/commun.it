b280d14ea8ddb690f3f4064c20e5945ee757170e
Pedro Costa <pedrorebelocosta@gmail.com>
2021-04-11 18:37:50 +0100

    change app icon, create add note activity

--- app/src/main/java/com/cpe/communit/activity/AddNoteActivity.kt
+++ app/src/main/java/com/cpe/communit/activity/AddNoteActivity.kt
@@ -3,12 +3,10 @@ package com.cpe.communit.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.cpe.communit.R
-import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
+class AddNoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-        login_back_button.setOnClickListener { onBackPressed() }
+        setContentView(R.layout.activity_add_note)
     }
 }
--- app/src/main/java/com/cpe/communit/activity/MainActivity.kt
+++ app/src/main/java/com/cpe/communit/activity/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         profile_card.setOnClickListener {
-            TODO("Needs verification on login status")
+            startActivity(Intent(this, LoginActivity::class.java))
         }
     }
 }
